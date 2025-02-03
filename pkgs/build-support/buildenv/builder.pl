866d24ea9655653e0e60cedd1b73b43f4b8a0400
shill-crestron <46201748+shill-crestron@users.noreply.github.com>
2025-02-03 08:32:02 -0800

    Update builder.pl
    Don't remove log folders

--- pkgs/build-support/buildenv/builder.pl
+++ pkgs/build-support/buildenv/builder.pl
@@ -129,7 +129,7 @@ sub findFiles {
         $relName =~ /info\/dir$/ ||
         ( $relName =~ /^\/share\/mime\// && !( $relName =~ /^\/share\/mime\/packages/ ) ) ||
         $baseName eq "perllocal.pod" ||
-        $baseName eq "log" ||
+        #$baseName eq "log" ||
         ! (hasPathsToLink($relName) || isInPathsToLink($relName));
 
     my ($oldTarget, $oldPriority) = @{$symlinks{$relName} // [undef, undef]};
